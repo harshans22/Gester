04a2478358d0409b4fef6e6340926e2f34b919dd
harshans22 <harshhub.414@gmail.com>
2024-08-21 02:37:26 +0530

    testing

--- lib/view/home/screens/homescreen.dart
+++ lib/view/home/screens/homescreen.dart
@@ -236,7 +236,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                   
                   const Gap(30),
-                   Text(' $timeRefrence Menu',
+                   Text(' $timeRefrence menu',
                    
                       style:const TextStyle(
                         fontSize: 18,
